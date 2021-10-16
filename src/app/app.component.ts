f404befc0cbefa2907d9142c4587d31431d9d500
Isac Bandeira <isac.bandeir4@gmail.com>
2021-10-16 14:07:46 -0300

    Conceitos fortes
    String Interpolations
    Pipes
    Pipes Custom
    NgIf
    NgFor
    Etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ProjetoAngular';
+  pageTitle = 'Acme Product Management';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ProductListComponent} from "./products/product-list.component";
+import {FormsModule} from "@angular/forms";
+import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
